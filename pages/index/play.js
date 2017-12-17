3689aee8dc471e123356c7d94c8130f3b23afca4
muxiaoqi <171084131@qq.com>
2017-12-17 23:43:12 +0800

    add scroll lyric

--- pages/index/play.js
+++ pages/index/play.js
@@ -16,7 +16,9 @@ Page({
       method: 'pause'
     },
     name: '',
-    author: ''
+    author: '',
+    musicLyric: {},
+    toView: ''
   },
 
   /**
@@ -59,15 +61,46 @@ Page({
         })
       }
     })
-
-    
+    // 获取歌词信息
+    wx.request({
+      url: mkPlayer.api,
+      data: {types: 'lyric', source: options.source, id: options.lyric_id},
+      method: 'get',
+      dataType: 'json',
+      success: function (e) {
+        if (e.data.lyric) {
+          var lyricObj = '';
+          console.log(e.data.lyric)
+          lyricObj = that.parseLyric(e.data.lyric);
+          that.setData({
+            musicLyric: lyricObj
+          })
+        }
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+        console.error(XMLHttpRequest + textStatus + errorThrown);
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
+  },
 
+  audioPlay: function () {
+    this.audioCtx.play()
+  },
+
+  audioPause: function () {
+    this.audioCtx.pause()
+  },
+
+  audioStart: function () {
+    this.audioCtx.seek(0)
   },
 
   /**
@@ -111,9 +144,34 @@ Page({
   onShareAppMessage: function () {
 
   },
-  navigateBack: function (e) {
-    var self = this;
-    var pages = getCurrentPages();
-    wx.navigateBack({ changed: true });
+
+  parseLyric: function (lrc) {
+    if (lrc === '') return '';
+    var lyrics = lrc.split("\n");
+    var lrcObj = {};
+    for (var i = 0; i < lyrics.length; i++) {
+      var lyric = decodeURIComponent(lyrics[i]);
+      var timeReg = /\[\d*:\d*((\.|\:)\d*)*\]/g;
+      var timeRegExpArr = lyric.match(timeReg);
+      if (!timeRegExpArr) continue;
+      var clause = lyric.replace(timeReg, '');
+      for (var k = 0, h = timeRegExpArr.length; k < h; k++) {
+        var t = timeRegExpArr[k];
+        var min = Number(String(t.match(/\[\d*/i)).slice(1)),
+          sec = Number(String(t.match(/\:\d*/i)).slice(1));
+        var time = min * 60 + sec;
+        lrcObj[time] = clause;
+      }
+    }
+    return lrcObj;
+  },
+
+  scrollLyc: function (e) {
+    var time = 0;
+    // console.log(e)
+    time = parseInt(e.detail.currentTime);
+    this.setData({
+      'toView': 'lyc_'+time
+    })
   }
 })
